08cc31a9e1680d2e1057e24288c068c786bd763a
Quốc Trần <168074688+Tranquoc2008@users.noreply.github.com>
2025-04-14 12:54:10 +0700

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,9 +1,16 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-58B9ZW2H07"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+// analytics.js
 
-  gtag('config', 'G-58B9ZW2H07');
-</script>
+// Nạp Google Tag Manager script
+const gtagScript = document.createElement('script');
+gtagScript.async = true;
+gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-58B9ZW2H07';
+document.head.appendChild(gtagScript);
+
+// Cấu hình gtag
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-58B9ZW2H07');
