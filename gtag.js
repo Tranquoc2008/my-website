dbdb7255da09e3a2f6895441b508d37edbbc5ac4
Quốc Trần <168074688+Tranquoc2008@users.noreply.github.com>
2025-04-14 12:43:40 +0700

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-58B9ZW2H07"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-58B9ZW2H07');
+</script>
